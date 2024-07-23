7f336b6d8bb11a1e41230e6b751f62e1f4b5b17b
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2024-07-23 19:04:50 +0200

    Update files

--- ls-17_over.sql
+++ ls-17_over.sql
@@ -77,4 +77,66 @@ join city c2
 GROUP by Name 
 ;
 
+-- Написать SQL-запрос для выбора городов, 
+-- занимающих первое место по населению в своих странах и 
+-- превышающих среднее население по всем городам.
+SELECT 
+*
+FROM 
+(
+SELECT 
+	c.Name as n_country,
+	c2.Name as n_city,
+	c2.Population as population,
+	c.LifeExpectancy as le,
+	max(c2.Population) over(PARTITION by c.Name ) as m_p_c,
+	ROUND(avg(c.LifeExpectancy) over(),1) as a_lf
+FROM country c 
+join city c2 
+	on c.Code = c2.CountryCode
+order by n_country , n_city 
+) as tb1
+where population = m_p_c
+and le > a_lf
+;
+
+-- Выведите список форм правления (GovernmentForm) 
+-- c количеством стран, где есть эта форма правления.
+SELECT 
+	Name,
+	GovernmentForm,
+	count(GovernmentForm) over (PARTITION BY GovernmentForm) as cnt,
+	COUNT(Name) over (PARTITION BY Name) as cnt_c
+FROM country c
+order by GovernmentForm 
+;
+
+-- Выведите второго по зарплате сотрудника 
+-- в каждом департаменте.
+use hr;
+select	database();
+	
+SELECT
+	*
+FROM
+	(
+select
+	d.department_id,
+	e.last_name,
+	e.salary,
+	dense_rank() over( PARTITION BY d.department_id
+ORDER BY
+	e.salary DESC) as rnk
+from
+	departments d
+left join employees e 
+	on
+	d.department_id = e.department_id
+) as tb1
+-- where rnk in (1,2)
+where rnk in (2)
+;
+
+
+
 
